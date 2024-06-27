1972284258482be31bf8b28051dae0600c2ca21a
danielluis07 <bombdaniel@hotmail.com>
2024-06-27 15:05:07 -0300

    changed cors origin

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -28,7 +28,7 @@ app.use(
 app.use(
   "*",
   cors({
-    origin: "http://localhost:3001",
+    origin: [process.env.NEXT_PUBLIC_APP_URL!, "http://localhost:3001"],
     allowMethods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
     allowHeaders: ["Content-Type", "Authorization"],
   })
