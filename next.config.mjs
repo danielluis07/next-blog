94371da9dce3d7ae9367a86059cd717cc487d0d0
danielluis07 <bombdaniel@hotmail.com>
2024-08-18 10:35:50 -0300

    allowed all origins on next config

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
           { key: "Access-Control-Allow-Credentials", value: "true" },
           {
             key: "Access-Control-Allow-Origin",
-            value: "http://localhost:3001",
+            value: "*",
           },
           {
             key: "Access-Control-Allow-Methods",
